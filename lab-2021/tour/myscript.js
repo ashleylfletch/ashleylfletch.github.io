6b4e6e3fe7284c13da2c702ea595c10f77c59665
ashleylfletch <fletc806@newschool.edu>
2021-03-29 20:54:58 -0400

    lots of stuff

--- lab-2021/tour/myscript.js
+++ lab-2021/tour/myscript.js
@@ -1,62 +1,88 @@
-<script type="text/javascript">
-//1
-//spin image on hover
+console.log("hi");
 
-//2
+//Page 1
+//JS Goal: spin image on hover
+var looper;
+var degrees = 0;
+function rotateAnimation(el,speed){
+	var elem = document.getElementById(el);
+	if(navigator.userAgent.match("Chrome")){
+		elem.style.WebkitTransform = "rotate("+degrees+"deg)";
+	} else if(navigator.userAgent.match("Firefox")){
+		elem.style.MozTransform = "rotate("+degrees+"deg)";
+	} else if(navigator.userAgent.match("MSIE")){
+		elem.style.msTransform = "rotate("+degrees+"deg)";
+	} else if(navigator.userAgent.match("Opera")){
+		elem.style.OTransform = "rotate("+degrees+"deg)";
+	} else {
+		elem.style.transform = "rotate("+degrees+"deg)";
+	}
+	looper = setTimeout('rotateAnimation(\''+el+'\','+speed+')',speed);
+	degrees++;
+	if(degrees > 359){
+		degrees = 1;
+	}
+	document.getElementById("status").innerHTML = "rotate("+degrees+"deg)";
+}
+
+//Page 2
 //image moves from left to right
-var width = $(window).width();
-var height = $(window).height();
-var upleft = 0;
-function goRight() {
-    $("#animate").animate({
-    left: width
-  }, 5000, function() {
-     setTimeout(goLeft, 50);
-  });
+dragElement(document.getElementById("img2"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
-function goLeft() {
-    $("#animate").animate({
-    left: 0
-  }, 5000, function() {
-     setTimeout(goRight, 50);
-  });
-}
-function goUp() {
-    $("#animate").animate({
-    top: 0
-  }, 5000, function() {
-     setTimeout(goDown, 50);
-  });
-}
-function goDown() {
-    $("#animate").animate({
-    top: height
-  }, 5000, function() {
-     setTimeout(goUp, 50);
-  });
-}
-setTimeout(goRight, 50);
 
-$("#animate").on('click',function(){$('#animate').stop();
-              if(upleft == 0)
-              {goDown(); upleft=1;}
-                                else {goLeft();upleft=0}
-})
+//Page 3
+//JS Goal: entire page fades to black then back to normal
 
-//3
-//
+//Page 4
 
-//4
+//Page 5
 
-//5
+//Page 6
 
-//6
+//Page 7
 
-//7
+//Page 8
 
-//8
+//Page 9
 
-//9
-
-//10
-</script>
+//Page 10
