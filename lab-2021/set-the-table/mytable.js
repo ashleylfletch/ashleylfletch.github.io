02f17a4328f157997e1ba0ba5e4d60bd5263ee7c
ashleylfletch <fletc806@newschool.edu>
2021-04-09 16:57:55 -0400

    updates

--- lab-2021/set-the-table/mytable.js
+++ lab-2021/set-the-table/mytable.js
@@ -8,106 +8,50 @@ var base = new Airtable({ apiKey: "key13Xr8YZ7doV9Tg" }).base(
 // https://airtable.com/api?utm_source=google&utm_medium=cpc&utm_extra5=kwd-826617918193&utm_extra2=936407691&utm_extra10=110555501161&creative=465926015426&device=c&cx=us&targetid=kwd-826617918193&campaignid=936407691&adgroupid=110555501161&utm_campaign=brand_creator&utm_content=bofu_freetrial&gclid=Cj0KCQjwmIuDBhDXARIsAFITC_5ScBkfOcfy68SuLtNsXhLoTp8JoYwFlBuOK6yDpmKnA_eCAXZ3kKsaAptkEALw_wcB
 
 let allItems = [];
+let allWineItems = [];
 
-base('my-dinner-table').select({}).eachPage(function page(tableItems, fetchNextPage) {
+base('my_dinner_table').select({}).eachPage(function page(tableItems, fetchNextPage) {
   tableItems.forEach(function(item) {
     allItems.push(item);
   });
 
+  fetchNextPage();
+
+}, function done(err) {
+  if (err) { console.error(err); return; }
 
   setTable(allItems);
 });
 
+let container = document.createElement("div");
+container.classList.add("container");
+document.body.appendChild(container);
 
 function setTable(allItems) {
-  console.log(allItems);
-    let container = document.createElement("div");
-  container.classList.add("container");
-  document.body.appendChild(container);
 
   allItems.forEach(function(item) {
-    console.log(item);
-      let name = item.fields.items;
-    console.log(name);
+    let name = item.fields.items;
     let imageUrl = item.fields.images[0].url;
     let itemImage = document.createElement('img');
+    itemImage.src = imageUrl;
+    itemImage.classList.add(item.fields.class_name);
+    container.appendChild(itemImage);
 
-    if (name === "Wine") {
-      for (var i=0; i<2; i++) {
-        let wine = document.createElement('img');
-        wine.src = imageUrl;
-        wine.classList.add("wine");
-
-        if (i === 0) {
-          wine.style.left = "70%";
-          wine.style.top = "-15%";
-        }
-        if (i === 1) {
-          wine.style.left = "25%";
-          wine.style.top = "30%";
-        }
-
-        container.appendChild(wine);
-      }
+    if (item.fields.kind_of_item == "drink") {
+      allWineItems.push(item);
     }
+  })
+}
 
-    if (name === "Plate") {
-      for (var i=0; i<2; i++) {
-        let plate = document.createElement('img');
-        plate.src = imageUrl;
-        plate.classList.add("plate");
+let button = document.getElementById("button");
+button.addEventListener('click', showWine);
 
-        if (i === 0) {
-          plate.style.left = "70%";
-          plate.style.top = "40%";
-        }
-        if (i === 1) {
-          plate.style.left = "4%";
-          plate.style.top = "40%";
-        }
-
-        container.appendChild(plate);
-      }
-    }
-
-    if (name === "Lasagna") {
-      for (var i=0; i<2; i++) {
-        let lasagna = document.createElement('img');
-        lasagna.src = imageUrl;
-        lasagna.classList.add("lasagna");
-
-        if (i === 0) {
-          lasagna.style.left = "75%";
-          lasagna.style.top = "35%";
-        }
-        if (i === 1) {
-          lasagna.style.left = "10%";
-          lasagna.style.top = "35%";
-        }
-
-        container.appendChild(lasagna);
-      }
-    }
-
-    if (name === "Basket") {
-      let basket = document.createElement('img');
-      basket.src = imageUrl;
-      basket.classList.add("basket");
-      container.appendChild(basket);
-    }
-
-    if (name === "Bread") {
-      let bread = document.createElement('img');
-      bread.src = imageUrl;
-      bread.classList.add("bread");
-      container.appendChild(bread);
-    }
-
-    if (name === "Tablecloth") {
-      let tablecloth = document.createElement('img');
-      tablecloth.src = imageUrl;
-      tablecloth.classList.add("tablecloth");
-      container.appendChild(tablecloth);
-    }
+function showWine() {
+  allWineItems.forEach(function(wineItem) {
+    let wine = document.createElement('img');
+    wine.src = wineItem.fields.images[0].url;
+    wine.classList.add(wineItem.fields.class_name);
+    wine.style.display = "block";
+    container.appendChild(wine);
   })
 }
---
+++ studio-2021/project-3-drafts/7/script.js
@@ -0,0 +1,76 @@
+console.log('this is working');
+var Airtable = require("airtable");
+var base = new Airtable({ apiKey: "key13Xr8YZ7doV9Tg" }).base(
+  "appFhx8BNwmUKTTwE"
+);
+
+let allItems = [];
+
+base('sarah-paulson').select({}).eachPage(function page(collectionItems, fetchNextPage) {
+  collectionItems.forEach(function(item) {
+    allItems.push(item);
+  });
+
+  displayCollection(allItems);
+});
+
+let allImages = [];
+function displayCollection(allItems) {
+    let container = document.createElement("div");
+  container.classList.add("container");
+  document.body.appendChild(container);
+
+  allItems.forEach(function(item) {
+      let season = item.fields.season;
+    allImages.push(item.fields.screenshot[0].url);
+    // let imageUrl = item.fields.images[0].url;
+    // let itemImage = document.createElement('img');
+    // itemImage.src = imageUrl;
+    // itemImage.classList.add(item.fields.season);
+    // container.appendChild(itemImage);
+
+    // if (season === "screenshot") {
+    //   let screenshot = document.createElement('img');
+    //   screenshot.src = imageUrl;
+    //   screenshot.classList.add("screenshot");
+    //   screenshot.appendChild(screenshot);
+    // }
+  })
+}
+//
+//
+//Controls
+//Sound
+function play() {
+ var audio = document.getElementById("audio");
+ audio.play();
+ audio.currentTime = 5;
+}
+
+// //Slider
+var slider = document.getElementById("myRange");
+
+slider.oninput = function() {
+  console.log(this.value);
+
+let sliderValue = this.value;
+  let tv = document.getElementById("tv");
+  if (sliderValue === "1") {
+    tv.style.backgroundImage = 'url(' + allImages[1] + ')';
+    document.getElementById("season").innerHTML = "Cult";
+    document.getElementById("episode").innerHTML = "1";
+    document.getElementById("timestamp-number").innerHTML = "00:01";
+  }
+  if (sliderValue === "2") {
+    tv.style.backgroundImage = 'url(' + allImages[2] + ')';
+    document.getElementById("season").innerHTML = "Asylum";
+    document.getElementById("episode").innerHTML = "2";
+    document.getElementById("timestamp-number").innerHTML = "00:02";
+  }
+  if (sliderValue === "3") {
+    tv.style.backgroundImage = 'url(' + allImages[3] + ')';
+    document.getElementById("season").innerHTML = "Freak Show";
+    document.getElementById("episode").innerHTML = "3";
+    document.getElementById("timestamp-number").innerHTML = "00:03";
+  }
+}
