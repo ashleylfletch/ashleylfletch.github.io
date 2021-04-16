7f9406c4eba53908df66acbd839aef46bd699310
ashleylfletch <fletc806@newschool.edu>
2021-04-16 11:02:59 -0400

    bubbles

---
+++ lab-2021/bubbles/script.js
@@ -0,0 +1,30 @@
+console.log("hello, Ashley, your Javascript is working if you can see this message.")
+
+function placeBubbles() {
+  // get all of the html elements that have the class bubble that ive written
+  let bubbles = document.querySelectorAll(".bubble");
+  // i know there are 6 bubbles, i want to position EACH of those bubbles in a different PLACE on the page
+  bubbles.forEach(function(bubble) { //do something to each bubble
+    let randomHorizontal = Math.floor(Math.random() * window.innerWidth); // give random whole number between 0 and page width
+    let randomVertical = Math.floor(Math.random() * window.innerHeight); // give random whole number between 0 and page height
+    bubble.style.top = randomVertical + "px";
+    bubble.style.left = randomHorizontal + "px";
+  })
+}
+
+document.addEventListener("keydown", addBubble);
+
+function addBubble(event) {
+  if (event.key === "b") {
+    let newBubble = document.createElement("div");
+    newBubble.classList.add("bubble");
+    document.body.appendChild(newBubble);
+    placeBubbles();
+  } else {
+    console.log("user pressed other key");
+  }
+}
+
+
+// call functions
+placeBubbles();
