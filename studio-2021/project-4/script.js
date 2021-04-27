bcd7353f69878c06e7dfca812b235ff1c0f5bf95
ashleylfletch <fletc806@newschool.edu>
2021-04-26 23:31:08 -0400

    part 2

---
+++ studio-2021/project-4/script.js
@@ -0,0 +1,25 @@
+//rotate handler
+const rotateHand = (event)=>{
+    const elem = event.target;
+    elem.style.cursor = "grabbing";
+    let rotating = true;
+    const radius = 252 / 2;
+    const rotateHandler = (e)=>{
+        const radians = Math.atan2(e.pageX - radius, e.pageY - radius);
+        let rotateDegrees = (radians * (180 / Math.PI) * -1) -180;
+        if (rotating) {
+            elem.style.transform = `rotate(${rotateDegrees}deg)`;
+        }
+    };
+    document.addEventListener("mousemove", rotateHandler);
+    const cancelRotate = (event)=>{
+        elem.style.cursor = "grab";
+        rotating = !rotating;
+        document.removeEventListener("mousemove", rotateHandler);
+        document.removeEventListener("mouseup", cancelRotate);
+
+    };
+    document.addEventListener("mouseup", cancelRotate);
+};
+
+document.querySelector(".gamut__timePicker__hrHand").addEventListener("mousedown", rotateHand);
