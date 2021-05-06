8960bd7c5eaee70d7f3486bb9ff7084f1932420e
ashleylfletch <fletc806@newschool.edu>
2021-05-06 16:16:11 -0400

    project 4 completed

--- studio-2021/project-4/script.js
+++ studio-2021/project-4/script.js
@@ -1,214 +1,225 @@
 var hand = document.getElementById("hand");
+var wind = document.getElementById("wind");
 var one = document.getElementById("one");
-  var click_one = true;
 var two = document.getElementById("two");
-  var click_two = true;
 var three = document.getElementById("three");
-  var click_three = true;
 var four = document.getElementById("four");
-  var click_four = true;
 var five = document.getElementById("five");
-  var click_five = true;
 var six = document.getElementById("six");
-  var click_six = true;
 var seven = document.getElementById("seven");
-  var click_seven = true;
 var eight = document.getElementById("eight");
-  var click_eight = true;
 var nine = document.getElementById("nine");
-  var click_nine = true;
 var ten = document.getElementById("ten");
-  var click_ten = true;
 var eleven = document.getElementById("eleven");
-  var click_eleven = true;
 var twelve = document.getElementById("twelve");
-  var click_twelve = true;
 
 one.onclick = function() {
   hand.style.transform = "rotate(30deg)";
   document.body.style.backgroundColor = "#e1f8ff";
-  if (click_one) {
-      document.getElementById("king").style.display = "block";
-      click_one = false;
-    } else {
-      document.getElementById("king").style.display = "none";
-      click_one = true;
-    }
+  document.getElementById("king").style.display = "block";
 }
 
 two.onclick = function() {
   hand.style.transform = "rotate(60deg)";
   document.body.style.backgroundColor = "#e1f8ff";
-  if (click_two) {
-      document.getElementById("queen").style.display = "block";
-      click_two = false;
-    } else {
-      document.getElementById("queen").style.display = "none";
-      click_two = true;
-    }
+  document.getElementById("queen").style.display = "block";
 }
 
 three.onclick = function() {
   hand.style.transform = "rotate(90deg)";
   document.body.style.backgroundColor = "#e1f8ff";
-  if (click_three) {
-      document.getElementById("egg").style.display = "block";
-      click_three = false;
-    } else {
-      document.getElementById("egg").style.display = "none";
-      click_three = true;
-    }
+  document.getElementById("princess").style.display = "block";
 }
 
 four.onclick = function() {
   hand.style.transform = "rotate(120deg)";
   document.body.style.backgroundColor = "#c9ebff";
-  if (click_four) {
-      document.getElementById("bunny").style.display = "block";
-      click_four = false;
-    } else {
-      document.getElementById("bunny").style.display = "none";
-      click_four = true;
-    }
+  document.getElementById("bunny").style.display = "block";
 }
 
 five.onclick = function() {
   hand.style.transform = "rotate(150deg)";
   document.body.style.backgroundColor = "#c9ebff";
-  if (click_five) {
-      document.getElementById("snail").style.display = "block";
-      click_five = false;
-    } else {
-      document.getElementById("snail").style.display = "none";
-      click_five = true;
-    }
+  document.getElementById("mushroom1").style.display = "block";
+  document.getElementById("mushroom2").style.display = "block";
+  document.getElementById("mushroom3").style.display = "block";
+  document.getElementById("mushroom4").style.display = "block";
+  document.getElementById("mushroom5").style.display = "block";
 }
 
 six.onclick = function() {
   hand.style.transform = "rotate(180deg)";
   document.body.style.backgroundColor = "#c9ebff";
-  if (click_six) {
-      document.getElementById("cloud1").style.display = "block";
-      document.getElementById("cloud2").style.display = "block";
-      document.getElementById("cloud3").style.display = "block";
-      click_six = false;
-    } else {
-      document.getElementById("cloud1").style.display = "none";
-      document.getElementById("cloud2").style.display = "none";
-      document.getElementById("cloud3").style.display = "none";
-      click_six = true;
-    }
+  document.getElementById("cloud1").style.display = "block";
+  document.getElementById("cloud2").style.display = "block";
+  document.getElementById("cloud3").style.display = "block";
 }
 
 seven.onclick = function() {
   hand.style.transform = "rotate(210deg)";
-  document.body.style.backgroundColor = "#abb5ff";
+  document.body.style.backgroundColor = "#7475b6";
+  document.getElementById("egg").style.display = "block";
 }
 
 eight.onclick = function() {
   hand.style.transform = "rotate(240deg)";
-  document.body.style.backgroundColor = "#abb5ff";
+  document.body.style.backgroundColor = "#7475b6";
+  document.getElementById("snail").style.display = "block";
 }
 
 nine.onclick = function() {
   hand.style.transform = "rotate(270deg)";
   document.body.style.backgroundColor = "#7475b6";
+  document.getElementById("witch").style.display = "block";
 }
 
 ten.onclick = function() {
   hand.style.transform = "rotate(300deg)";
-  document.body.style.backgroundColor = "#7475b6";
-  if (click_ten) {
-      document.getElementById("witch").style.display = "block";
-      click_ten = false;
-    } else {
-      document.getElementById("witch").style.display = "none";
-      click_ten = true;
-    }
+  document.body.style.backgroundColor = "#5148b2";
+  document.getElementById("star1").style.display = "block";
+  document.getElementById("star2").style.display = "block";
+  document.getElementById("star3").style.display = "block";
+  document.getElementById("star4").style.display = "block";
+  document.getElementById("star5").style.display = "block";
+  document.getElementById("star6").style.display = "block";
 }
 
 eleven.onclick = function() {
   hand.style.transform = "rotate(330deg)";
   document.body.style.backgroundColor = "#5148b2";
-  if (click_eleven) {
-      document.getElementById("star1").style.display = "block";
-      document.getElementById("star2").style.display = "block";
-      document.getElementById("star3").style.display = "block";
-      document.getElementById("star4").style.display = "block";
-      document.getElementById("star5").style.display = "block";
-      document.getElementById("star6").style.display = "block";
-      click_eleven = false;
-    } else {
-      document.getElementById("star1").style.display = "none";
-      document.getElementById("star2").style.display = "none";
-      document.getElementById("star3").style.display = "none";
-      document.getElementById("star4").style.display = "none";
-      document.getElementById("star5").style.display = "none";
-      document.getElementById("star6").style.display = "none";
-      click_eleven = true;
-    }
-}
-
-twelve.onclick = function() {
-  hand.style.transform = "rotate(0deg)";
-  document.body.style.backgroundColor = "#5148b2";
+  document.getElementById("left-eye-closed").style.display = "block";
+  document.getElementById("right-eye-closed").style.display = "block";
+  document.getElementById("zzz").style.display = "block";
+  document.getElementById("left-eye").style.display = "none";
+  document.getElementById("right-eye").style.display = "none";
   document.getElementById("king").style.display = "none";
   document.getElementById("queen").style.display = "none";
+  document.getElementById("princess").style.display = "none";
   document.getElementById("egg").style.display = "none";
   document.getElementById("bunny").style.display = "none";
-  document.getElementById("snail").style.display = "none";
-  document.getElementById("witch").style.display = "none";
+  document.getElementById("mushroom1").style.display = "none";
+  document.getElementById("mushroom2").style.display = "none";
+  document.getElementById("mushroom3").style.display = "none";
+  document.getElementById("mushroom4").style.display = "none";
+  document.getElementById("mushroom5").style.display = "none";
   document.getElementById("cloud1").style.display = "none";
   document.getElementById("cloud2").style.display = "none";
   document.getElementById("cloud3").style.display = "none";
+  document.getElementById("snail").style.display = "none";
+  document.getElementById("witch").style.display = "none";
   document.getElementById("star1").style.display = "none";
   document.getElementById("star2").style.display = "none";
   document.getElementById("star3").style.display = "none";
   document.getElementById("star4").style.display = "none";
   document.getElementById("star5").style.display = "none";
   document.getElementById("star6").style.display = "none";
-  if (click_twelve) {
-      document.getElementById("left-eye-closed").style.display = "block";
-      document.getElementById("right-eye-closed").style.display = "block";
-      document.getElementById("left-eye").style.display = "none";
-      document.getElementById("right-eye").style.display = "none";
-      click_twelve = false;
-    } else {
-      document.getElementById("left-eye-closed").style.display = "none";
-      document.getElementById("right-eye-closed").style.display = "none";
-      document.getElementById("left-eye").style.display = "block";
-      document.getElementById("right-eye").style.display = "block";
-      click_twelve = true;
-    }
 }
 
-// let wind = document.getElementById("wind");
-// let number = 0;
-// let playing = false;
-// let speedValue = 1000;
-//
-// function autoPlay () {
-//   if (number < 12 && playing === true) {
-//     setTimeout(() => {
-//       number = number + 1;
-//   if (number = 1) {
-//     hand.style.transform = "rotate(30deg)";
-//     document.body.style.backgroundColor = "#e1f8ff";
-//     document.getElementById("king").style.display = "block";
-//   }
-//   if (number = 2) {
-//     hand.style.transform = "rotate(60deg)";
-//     document.body.style.backgroundColor = "#e1f8ff";
-//     document.getElementById("queen").style.display = "block";
-//   }
-//   if (number = 3) {
-//     hand.style.transform = "rotate(90deg)";
-//     document.body.style.backgroundColor = "#e1f8ff";
-//     document.getElementById("egg").style.display = "block";
-//   }
-// }
-//
-// wind.onclick = function {
-//   playing = true;
-//   autoPlay();
-// }
+twelve.onclick = function() {
+  hand.style.transform = "rotate(0deg)";
+  document.body.style.backgroundColor = "#e1f8ff";
+  document.getElementById("king").style.display = "none";
+  document.getElementById("queen").style.display = "none";
+  document.getElementById("princess").style.display = "none";
+  document.getElementById("egg").style.display = "none";
+  document.getElementById("bunny").style.display = "none";
+  document.getElementById("mushroom1").style.display = "none";
+  document.getElementById("mushroom2").style.display = "none";
+  document.getElementById("mushroom3").style.display = "none";
+  document.getElementById("mushroom4").style.display = "none";
+  document.getElementById("mushroom5").style.display = "none";
+  document.getElementById("cloud1").style.display = "none";
+  document.getElementById("cloud2").style.display = "none";
+  document.getElementById("cloud3").style.display = "none";
+  document.getElementById("snail").style.display = "none";
+  document.getElementById("witch").style.display = "none";
+  document.getElementById("star1").style.display = "none";
+  document.getElementById("star2").style.display = "none";
+  document.getElementById("star3").style.display = "none";
+  document.getElementById("star4").style.display = "none";
+  document.getElementById("star5").style.display = "none";
+  document.getElementById("star6").style.display = "none";
+  document.getElementById("left-eye-closed").style.display = "none";
+  document.getElementById("right-eye-closed").style.display = "none";
+  document.getElementById("left-eye").style.display = "block";
+  document.getElementById("right-eye").style.display = "block";
+  document.getElementById("zzz").style.display = "none";
+}
+
+function showAbout () {
+    document.getElementById("about").style.display = "block";
+    document.getElementById("castle-inside").style.display = "block";
+    document.getElementById("grass-container").style.display = "none";
+    document.getElementById("hand").style.display = "none";
+    document.getElementById("door-inside1").style.display = "block";
+    document.getElementById("door-inside2").style.display = "block";
+    document.getElementById("door-outside1").style.display = "none";
+    document.getElementById("door-outside2").style.display = "none";
+    document.getElementById("king-inside").style.display = "block";
+    document.getElementById("queen-inside").style.display = "block";
+    document.getElementById("princess-inside").style.display = "block";
+    document.getElementById("bunny-inside").style.display = "block";
+    document.getElementById("mushroom-inside").style.display = "block";
+    document.getElementById("cloud-inside").style.display = "block";
+    document.getElementById("egg-inside").style.display = "block";
+    document.getElementById("snail-inside").style.display = "block";
+    document.getElementById("witch-inside").style.display = "block";
+    document.getElementById("star-inside").style.display = "block";
+    document.getElementById("left-eye").style.display = "none";
+    document.getElementById("right-eye").style.display = "none";
+    document.getElementById("king").style.display = "none";
+    document.getElementById("queen").style.display = "none";
+    document.getElementById("princess").style.display = "none";
+    document.getElementById("egg").style.display = "none";
+    document.getElementById("bunny").style.display = "none";
+    document.getElementById("mushroom1").style.display = "none";
+    document.getElementById("mushroom2").style.display = "none";
+    document.getElementById("mushroom3").style.display = "none";
+    document.getElementById("mushroom4").style.display = "none";
+    document.getElementById("mushroom5").style.display = "none";
+    document.getElementById("cloud1").style.display = "none";
+    document.getElementById("cloud2").style.display = "none";
+    document.getElementById("cloud3").style.display = "none";
+    document.getElementById("snail").style.display = "none";
+    document.getElementById("witch").style.display = "none";
+    document.getElementById("star1").style.display = "none";
+    document.getElementById("star2").style.display = "none";
+    document.getElementById("star3").style.display = "none";
+    document.getElementById("star4").style.display = "none";
+    document.getElementById("star5").style.display = "none";
+    document.getElementById("star6").style.display = "none";
+    document.getElementById("left-eye-closed").style.display = "none";
+    document.getElementById("right-eye-closed").style.display = "none";
+    document.getElementById("left-eye").style.display = "block";
+    document.getElementById("right-eye").style.display = "block";
+    document.getElementById("wind-show").style.display = "none";
+    document.getElementById("wind-hide").style.display = "block";
+    document.getElementById("zzz").style.display = "none";
+}
+
+function hideAbout () {
+    document.getElementById("about").style.display = "none";
+    document.getElementById("castle-inside").style.display = "none";
+    document.getElementById("grass-container").style.display = "block";
+    document.getElementById("hand").style.display = "block";
+    document.getElementById("door-inside1").style.display = "none";
+    document.getElementById("door-inside2").style.display = "none";
+    document.getElementById("door-outside1").style.display = "block";
+    document.getElementById("door-outside2").style.display = "block";
+    document.getElementById("king-inside").style.display = "none";
+    document.getElementById("queen-inside").style.display = "none";
+    document.getElementById("princess-inside").style.display = "none";
+    document.getElementById("bunny-inside").style.display = "none";
+    document.getElementById("mushroom-inside").style.display = "none";
+    document.getElementById("cloud-inside").style.display = "none";
+    document.getElementById("egg-inside").style.display = "none";
+    document.getElementById("snail-inside").style.display = "none";
+    document.getElementById("witch-inside").style.display = "none";
+    document.getElementById("star-inside").style.display = "none";
+    document.getElementById("left-eye-closed").style.display = "none";
+    document.getElementById("right-eye-closed").style.display = "none";
+    document.getElementById("left-eye").style.display = "block";
+    document.getElementById("right-eye").style.display = "block";
+    document.getElementById("wind-show").style.display = "block";
+    document.getElementById("wind-hide").style.display = "none";
+}
